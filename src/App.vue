b9788c6c1793243fd8c9b76a4f501efa4bdc5d90
LQS666 <p.turkot123@gmail.com>
2018-09-08 22:27:36 +0200

    Finished app

--- src/App.vue
+++ src/App.vue
@@ -9,8 +9,10 @@
     <Claim v-if="step === 0"/>
     <SearchInput v-model="searchValue" @input="handleInput" :dark="step === 1" />
     <div class="results" v-if="results && !loading && step === 1" >
-      <Item v-for="item in results" :item="item" :key="item.data[0].nasa_id" />
+      <Item v-for="item in results" :item="item" :key="item.data[0].nasa_id" @click.native="handleModalOpen(item)"/>
     </div>
+    <div class="loader" v-if="step === 1 && loading"></div>
+    <Modal v-if="modalOpen" :item="modalItem" @closeModal="modalOpen = false" />
   </div>
 </template>
 
@@ -22,6 +24,7 @@ import Claim from '@/components/Claim.vue';
 import SearchInput from '@/components/SearchInput.vue';
 import HeroImage from '@/components/HeroImage.vue';
 import Item from '@/components/Item.vue';
+import Modal from '@/components/Modal.vue';
 
 const API = 'https://images-api.nasa.gov/search';
 
@@ -32,9 +35,12 @@ export default {
     SearchInput,
     HeroImage,
     Item,
+    Modal,
   },
   data() {
     return {
+      modalOpen: false,
+      modalItem: null,
       loading: false,
       step: 0,
       searchValue: '',
@@ -42,6 +48,10 @@ export default {
     };
   },
   methods: {
+    handleModalOpen(item) {
+      this.modalOpen = true;
+      this.modalItem = item;
+    },
     // eslint-disable-next-line
       handleInput: debounce(function() {
       this.loading = true;
@@ -92,8 +102,7 @@ export default {
   .fade-enter-active, .fade-leave-active {
     transition: opacity .3s ease;
   }
-  .fade-enter, .fade-leave-to
-    /* .component-fade-leave-active below version 2.1.8 */ {
+  .fade-enter, .fade-leave-to {
     opacity: 0;
   }
 
@@ -105,8 +114,7 @@ export default {
   .slide-enter-active, .slide-leave-active {
     transition: margin-top .3s ease;
   }
-  .slide-enter, .slide-leave-to
-    /* .component-fade-leave-active below version 2.1.8 */ {
+  .slide-enter, .slide-leave-to {
     margin-top: -50px;
   }
 
@@ -118,6 +126,37 @@ export default {
 
     @media (min-width: 768px) {
       grid-template-columns: 1fr 1fr 1fr;
+    }
+  }
+
+  .loader {
+    margin-top: 100px;
+    display: inline-block;
+    width: 64px;
+    height: 64px;
+
+    @media (min-width: 768px) {
+      width: 90px;
+      height: 90px;
+    }
+  }
+  .loader:after {
+    content: " ";
+    display: block;
+    width: 46px;
+    height: 46px;
+    margin: 1px;
+    border-radius: 50%;
+    border: 5px solid #1e3d4a;
+    border-color: #1e3d4a transparent #1e3d4a transparent;
+    animation: loading 1.2s linear infinite;
+  }
+  @keyframes loading {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
     }
   }
 </style>
